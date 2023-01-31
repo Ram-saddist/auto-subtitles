8db45f84059b7112b8042308a8ba5890661f705f
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2023-01-31 11:51:48 +0530

    dowmload option added

--- subtitle.js
+++ subtitle.js
@@ -1,65 +1,53 @@
 const texts = document.querySelector(".texts");
-
+let bstop=true
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
-
 let p = document.createElement("p");
-
+p.setAttribute("class","textRead")
 recognition.addEventListener("result", (e) => {
   texts.appendChild(p);
   const text = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
     .join("");
-
   p.innerText = text;
-  /*if (e.results[0].isFinal) {
-    p = document.createElement("p");
-  }*/
 });
 
-recognition.addEventListener("end", () => {
-  recognition.start();
-});
+  recognition.addEventListener("end", () => {
+    if(bstop==true){
+      recognition.start();
+    }
+    else if(bstop==false){
+      recognition.abort()
+    }
+  });
 
-recognition.start();
-function stopConvert(){
-  recognition.stop();
-  console.log("stop the action")
+function startConvertion(){
+    recognition.start();
 }
-/*
-const texts=document.querySelector(".texts")
-
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-
-let p = document.createElement("p");
-
-recognition.addEventListener("result",(e)=>{
-  texts.appendChild(p);
-  const text = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join("");
-
-  p.innerText = text;
-  if (e.results[0].isFinal) {
-    p = document.createElement("p");
-  }
-})
-
-recognition.addEventListener("end", () => {
-  recognition.start();
-});
-
-recognition.start();
-
 function stopConvert(){
-  recognition.stop();
-}*/
+    bstop=false
+    recognition.abort();
+    textData()
+}
+function textData()
+{
+  let textRead=document.querySelector(".textRead").innerHTML
+  console.log(textRead)
+  console.log("from text data funtion")
+  const link = document.createElement("a");
+  // Create a blog object with the file content which you want to add to the file
+  const file = new Blob([textRead], { type: 'text/plain' });
+
+  // Add file content in the object URL
+  link.href = URL.createObjectURL(file);
+
+  // Add file name
+  link.download = "finalText.txt";
+
+  // Add click event to <a> tag to save file.
+  link.click();
+  URL.revokeObjectURL(link.href);
+}
